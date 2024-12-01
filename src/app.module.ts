b707ad51c6f46fca0bacb50f8eb21ea4d4542902
yossi78 <yossi78@gmail.com>
2024-12-01 18:19:51 +0200

    add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
-
-
-@Module({  
-  imports: [DatabaseModule] ,
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes .env configuration available globally
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-import { databaseProviders } from './database.providers';
-
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 
 @Module({
   imports: [
-    ...databaseProviders    
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Ensures ConfigModule is available
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Set to false in production
+      }),
+      inject: [ConfigService],
+    }),
   ],
 })
 export class DatabaseModule {}
